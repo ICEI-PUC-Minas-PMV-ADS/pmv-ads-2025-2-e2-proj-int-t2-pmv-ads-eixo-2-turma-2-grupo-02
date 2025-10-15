bf57da6f5e7771788f1d91bfed31bf1a13083116
Mateus de Lucena <mateustavares@protonmail.com>
2025-10-14 20:20:48 -0400

    feat: add input styles

--- src/AgrInov/Models/Insumo.cs
+++ src/AgrInov/Models/Insumo.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AgrInov.Models
 {
@@ -8,6 +9,7 @@ namespace AgrInov.Models
         public int Id { get; set; }
         public string Nome { get; set; }
         public float Quantidade { get; set; }
+        [DisplayName("Unidade Medida")]
         public string UnidadeMedida { get; set; }
     }
 }
