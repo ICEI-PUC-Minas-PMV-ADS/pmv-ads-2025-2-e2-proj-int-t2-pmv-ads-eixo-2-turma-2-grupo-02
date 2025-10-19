a9f45b686024492d7058a0ec5b71c1293e5196ca
mario guilherme <carvalho16car@gmail.com>
2025-10-19 17:42:16 -0400

    feat: add displayname

--- src/AgrInov/Models/PropriedadeAgricula.cs
+++ src/AgrInov/Models/PropriedadeAgricula.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AgrInov.Models
@@ -8,12 +9,20 @@ namespace AgrInov.Models
     {
         [Key]
         public int Id { get; set; }
+
+        [DisplayName("Área")]
         public float Area { get; set; }
+        
+        [DisplayName("Hectares")]
         public float TamanhoHectares { get; set; }
         [Required(ErrorMessage = "Obrigatório informar o CNPJ")]
         public string Cnpj { get; set; }
         public string Status { get; set; }
+
+        [DisplayName("Tipo de Solo")]
         public string TipoDeSolo { get; set; }
+
+        [DisplayName("Localização")]
         public string Localizacao { get; set; }
 
     }
