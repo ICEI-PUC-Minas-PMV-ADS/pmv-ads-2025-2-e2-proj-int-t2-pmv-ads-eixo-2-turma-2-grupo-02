0808666dc6bb2a0d620b213b8a4c55c7a20d54b2
Gapi2006 <gabriel.pires@sga.pucminas.br>
2025-11-04 22:04:24 -0300

    fix- plantação

--- src/AgrInov/Controllers/PlantaçaoController.cs
+++ src/AgrInov/Controllers/PlantaçaoController.cs
@@ -54,7 +54,7 @@ namespace AgrInov.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,DataInicio,DataFimPrevista,AreaUtilizada,Status,Producao,Saude")] Plantaçao plantaçao)
+        public async Task<IActionResult> Create([Bind("Id,DataInicio,DataFimPrevista,AreaUtilizada,Status,Producao,Saude")] Plantacao plantaçao)
         {
             if (ModelState.IsValid)
             {
@@ -86,7 +86,7 @@ namespace AgrInov.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,DataInicio,DataFimPrevista,AreaUtilizada,Status,Producao,Saude")] Plantaçao plantaçao)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,DataInicio,DataFimPrevista,AreaUtilizada,Status,Producao,Saude")] Plantacao plantaçao)
         {
             if (id != plantaçao.Id)
             {
