05d3659628194240ee436b6558848fff59b62ee2
Gapi2006 <gabriel.pires@sga.pucminas.br>
2025-11-04 21:48:10 -0300

    Adiciona CRUD para Plantações

---
+++ src/AgrInov/Controllers/PlantaçaoController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AgrInov.Data;
+using AgrInov.Models;
+
+namespace AgrInov.Controllers
+{
+    public class PlantaçaoController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public PlantaçaoController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Plantaçao
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Plantações.ToListAsync());
+        }
+
+        // GET: Plantaçao/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var plantaçao = await _context.Plantações
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (plantaçao == null)
+            {
+                return NotFound();
+            }
+
+            return View(plantaçao);
+        }
+
+        // GET: Plantaçao/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Plantaçao/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,DataInicio,DataFimPrevista,AreaUtilizada,Status,Producao,Saude")] Plantaçao plantaçao)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(plantaçao);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(plantaçao);
+        }
+
+        // GET: Plantaçao/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var plantaçao = await _context.Plantações.FindAsync(id);
+            if (plantaçao == null)
+            {
+                return NotFound();
+            }
+            return View(plantaçao);
+        }
+
+        // POST: Plantaçao/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,DataInicio,DataFimPrevista,AreaUtilizada,Status,Producao,Saude")] Plantaçao plantaçao)
+        {
+            if (id != plantaçao.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(plantaçao);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PlantaçaoExists(plantaçao.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(plantaçao);
+        }
+
+        // GET: Plantaçao/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var plantaçao = await _context.Plantações
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (plantaçao == null)
+            {
+                return NotFound();
+            }
+
+            return View(plantaçao);
+        }
+
+        // POST: Plantaçao/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var plantaçao = await _context.Plantações.FindAsync(id);
+            if (plantaçao != null)
+            {
+                _context.Plantações.Remove(plantaçao);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PlantaçaoExists(int id)
+        {
+            return _context.Plantações.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ src/AgrInov/Models/Plantaçao.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AgrInov.Models
+{
+    [Table("Plantações")]
+    public class Plantaçao
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime DataInicio { get; set; }
+        public DateTime DataFimPrevista { get; set; }
+        public float AreaUtilizada { get; set; }
+        public string Status { get; set; }
+        public int Producao { get; set; }
+        public string Saude  { get; set; }
+
+    }
+}
