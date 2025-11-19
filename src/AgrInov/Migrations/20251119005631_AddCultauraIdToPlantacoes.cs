1e73510847c837762ba2c49849bd7c75f5b3a475
Sutolix <tiago.reis.1590556@sga.pucminas.br>
2025-11-18 22:20:14 -0300

    feat(plantacoes): add association between Plantacao and Cultura

---
+++ src/AgrInov/Migrations/20251119005631_AddCultauraIdToPlantacoes.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AgrInov.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCultauraIdToPlantacoes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CulturaId",
+                table: "Plantacoes",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Plantacoes_CulturaId",
+                table: "Plantacoes",
+                column: "CulturaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Plantacoes_Culturas_CulturaId",
+                table: "Plantacoes",
+                column: "CulturaId",
+                principalTable: "Culturas",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Plantacoes_Culturas_CulturaId",
+                table: "Plantacoes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Plantacoes_CulturaId",
+                table: "Plantacoes");
+
+            migrationBuilder.DropColumn(
+                name: "CulturaId",
+                table: "Plantacoes");
+        }
+    }
+}
