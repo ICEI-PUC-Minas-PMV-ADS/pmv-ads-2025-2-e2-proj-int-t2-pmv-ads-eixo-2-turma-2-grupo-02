37c1b6b9485ddbb65aa633ad49ad2b21dc0444ef
Gapi2006 <gabriel.pires@sga.pucminas.br>
2025-11-18 21:34:57 -0300

    commit-previsãoClima

---
+++ src/AgrInov/Models/WeatherModels.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace AgrInov.Models
+{
+    public class PrevisaoDiaria
+    {
+        public string date { get; set; }
+        public string weekday { get; set; }
+        public int max { get; set; }
+        public int min { get; set; }
+        public string description { get; set; }
+    }
+
+    public class PrevisaoResultado
+    {
+        public List<PrevisaoDiaria> forecast { get; set; } = new List<PrevisaoDiaria>();
+        public string city_name { get; set; }
+    }
+
+    public class PrevisaoRaiz
+    {
+        public PrevisaoResultado results { get; set; }
+    }
+}
