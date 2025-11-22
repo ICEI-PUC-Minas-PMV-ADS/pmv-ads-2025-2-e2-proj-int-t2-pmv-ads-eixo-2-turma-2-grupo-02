5ff7ba668b7a49ccd974a2899e53c248a53f11c0
Sutolix <tiago.reis.1590556@sga.pucminas.br>
2025-11-22 09:00:43 -0300

    feat(plantacao): add support for Name and new relationships

--- src/AgrInov/Migrations/20251122111352_AddAreaDePlantioIdToPlantacoes.Designer.cs
+++ src/AgrInov/Migrations/20251122111352_AddAreaDePlantioIdToPlantacoes.Designer.cs
@@ -4,6 +4,7 @@ using AgrInov.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AgrInov.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251122111352_AddAreaDePlantioIdToPlantacoes")]
+    partial class AddAreaDePlantioIdToPlantacoes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -38,9 +41,6 @@ namespace AgrInov.Migrations
 
                     b.Property<DateTime>("DataInicio")
                         .HasColumnType("datetime2");
-
-                    b.Property<string>("Nome")
-                        .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("Status")
                         .HasColumnType("nvarchar(max)");
